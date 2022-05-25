41e380b8bc60196944d322dea9b0654a299d922f
iliebraiu <56258759+iliebraiu@users.noreply.github.com>
2022-05-25 18:56:46 +0300

    Add files via upload
    2.7

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let n = prompt('Indicati un numar', '');
 
-if (n > 0) {
+if (n > 14) {
   alert( '1' );
-} else if (n < 0) {
-  alert( '-1' );
+} else if (n < 90) {
+  alert( '1' );
 } else if(n == 0)  {
   alert( '0' );
- 
 } else {
   alert ('error');
 };
