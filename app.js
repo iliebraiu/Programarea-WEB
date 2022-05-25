16c8906f86271f8e91ad74bf93cc301181ff1b67
iliebraiu <56258759+iliebraiu@users.noreply.github.com>
2022-05-25 18:56:02 +0300

    Add files via upload
    2.6

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-let a = prompt('indicati a');
-let b = prompt('indicati b');
-let c = prompt('indicati c');
-let max ;
-let min ;
-if (a>b) {max = a; min = b;}
-else {max = b; min = a;}
-if (max<c) max = c; 
-if(min>c) min = c;
+let n = prompt('Indicati un numar', '');
 
-alert ('max = '+ max + ' min = ' + min );
+if (n > 0) {
+  alert( '1' );
+} else if (n < 0) {
+  alert( '-1' );
+} else if(n == 0)  {
+  alert( '0' );
+ 
+} else {
+  alert ('error');
+};
